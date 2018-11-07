1f338bc6fbec0025307fe5936b8edd1b0d7f516d
DrunkPsyduck <40211718+DrunkPsyduck@users.noreply.github.com>
2018-11-07 10:20:50 +0100

    Nuevos ejercicios
    Se añaden los ejercicios de Arrays 8, 9 y 10.

---
+++ EjerciciosArrays/src/Ejercicio10.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Ejercicio10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int [] numeros;
+		int suma;
+		
+		numeros = new int[10];
+		suma=0;
+		
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.println("Introduzca un numero (" + i + ")" );
+			numeros[i]=sc.nextInt();
+			
+		}
+		
+		for (int i = 0; i < numeros.length; i++) {
+			if (i%2==0) {
+				suma += numeros[i];
+			}
+		}
+		System.out.printf("La suma de las posiciones pares es 5", suma);
+		sc.close();
+	}
+
+}
