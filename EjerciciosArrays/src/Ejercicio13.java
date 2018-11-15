7d21dad2fb80aba47e99290882c1af442ad7dacc
DrunkPsyduck <40211718+DrunkPsyduck@users.noreply.github.com>
2018-11-15 18:04:57 +0100

    actualizacion1511
    Ejercicio13 resuelto

--- EjerciciosArrays/src/Ejercicio13.java
+++ EjerciciosArrays/src/Ejercicio13.java
@@ -1,25 +1,65 @@
 import java.util.Scanner;
+
 public class Ejercicio13 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
+
 		int numeroIntroducido;
-		int [] hexadecimal;
-		int [] decimal;
-		
-		
+		int restoNumero;
+		int divisionNumero;
+
+		int[] hexadecimal;
+		char[] salida;
+		char aux;
+
+		hexadecimal = new int[4];
+		salida = new char[4];
+
 		System.out.println("introduzca un número entre 0 y 32000 ");
 		numeroIntroducido = sc.nextInt();
-		
-		while (numeroIntroducido<0 || numeroIntroducido>32000) {
+
+		while (numeroIntroducido < 0 || numeroIntroducido > 32000) {
 			System.out.println("el numero es incorrecto. Vuelva a introducirlo");
 			numeroIntroducido = sc.nextInt();
 		}
-		
-		
-		
+
+		restoNumero = numeroIntroducido;
+		divisionNumero = numeroIntroducido;
+
+		int posicion;
+		for (posicion = 0; divisionNumero >= 16; posicion++) {
+			restoNumero %= 16;
+			hexadecimal[posicion] = restoNumero;
+			divisionNumero /= 16;
+		}
+
+		hexadecimal[posicion] = divisionNumero;
+
+		for (int i = 0; i < salida.length; i++) {
+			if (hexadecimal[i] < 10)
+				salida[i] = (char) (hexadecimal[i] + 48);
+			else
+				salida[i] = (char) (hexadecimal[i] + 55);
+		}
+
+		aux = salida[0];
+		salida[0] = salida[salida.length - 1];
+		salida[salida.length - 1] = aux;
+		aux = salida[1];
+		salida[1] = salida[salida.length - 2];
+		salida[salida.length - 2] = aux;
+
+		for (int j = 0; j < salida.length; j++) {
+
+			System.out.print(salida[j]);
+
+		}
+
+		// System.out.println(salida);
+
+		sc.close();
 	}
 
 }
--- EjerciciosArrays/src/Ejercicio14.java
+++ EjerciciosArrays/src/Ejercicio14.java
@@ -1,9 +1,4 @@
 
 public class Ejercicio14 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
