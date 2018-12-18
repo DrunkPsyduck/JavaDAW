6367e15554399c9bb5e80bc251213f9d22d49154
DrunkPsyduck <40211718+DrunkPsyduck@users.noreply.github.com>
2018-12-18 20:59:24 +0100

    Actualizacion 1812
    Actualización Menu1.
     Se añade el conversor a pulgadas y centimetros.

--- ProgramaciónModular/Menu1.java
+++ ProgramaciónModular/Menu1.java
@@ -1,29 +1,46 @@
 import java.util.Scanner;
 
 public class Menu1 {
-
+	/***
+	 *
+	 * @param a
+	 * @param b
+	 * @return a?=b
+	 */
 	public static int suma(int a, int b) {
-		int resultado;
-		resultado = a + b;
-		return resultado;
+		;
+		a += b;
+		return a;
 	}
 
 	public static int resta(int a, int b) {
-		int resultado;
-		resultado = a - b;
-		return resultado;
+
+		a -= b;
+		return a;
 	}
 
 	public static int multiplicar(int a, int b) {
-		int resultado;
-		resultado = a * b;
-		return resultado;
+
+		a *= b;
+		return a;
 	}
 
 	public static int dividir(int a, int b) {
-		int resultado;
-		resultado = a / b;
-		return resultado;
+
+		a /= b;
+		return a;
+	}
+
+	public static double pulgadas(double a) {
+		a *= 0.39370;
+		return a;
+
+	}
+
+	public static double centimetros(double a) {
+		a /= 0.3937;
+		return a;
+
 	}
 
 	public static void main(String[] args) {
@@ -34,18 +51,19 @@ public class Menu1 {
 		boolean fin = false;
 
 		do {
-			System.out.println("introduzca su opcion:\nCalculadora\nSalir");
+			System.out.println("introduzca su opcion:\nCalculadora\nConversor\nSalir");
 			opcion = sc.nextLine();
 			switch (opcion) {
 			case "Calculadora":
-				
-				System.out.println("Que operacion desea realizar");
+
+				System.out.println("Que operacion desea realizar\n Suma, Resta, Multiplicacion, Division");
 				String operacion = sc.nextLine();
 
-				int a = sc.nextInt();
-				int b = sc.nextInt();
+				int a;
+				int b;
 
-				switch (operacion) {				
+				switch (operacion) {
+				case "suma":
 				case "Suma":
 					System.out.println("Introduzca un numero");
 					a = sc.nextInt();
@@ -56,6 +74,8 @@ public class Menu1 {
 					System.out.println("El resultado de la suma es " + suma(a, b));
 
 					break;
+
+				case "resta":
 				case "Resta":
 					System.out.println("Introduzca un numero");
 					a = sc.nextInt();
@@ -65,6 +85,8 @@ public class Menu1 {
 
 					System.out.println("El resultado de la resta es " + resta(a, b));
 					break;
+
+				case "multiplicar":
 				case "Multiplicar":
 					System.out.println("Introduzca un numero");
 					a = sc.nextInt();
@@ -75,6 +97,8 @@ public class Menu1 {
 					System.out.println("El resultado de la multiplicacion es " + multiplicar(a, b));
 
 					break;
+
+				case "dividir":
 				case "Dividir":
 					System.out.println("Introduzca un numero");
 					a = sc.nextInt();
@@ -85,18 +109,45 @@ public class Menu1 {
 					System.out.println("El resultado de la division es " + dividir(a, b));
 
 					break;
+
 				default:
 					System.out.println("No especificado");
 				}
 
 				break;
+			case "conversor":
+			case "Conversor":
+				String medidas;
+				int distancia;
+				System.out.println("A que desea convertir. Centimetros o pulgadas");
+				medidas = sc.next();
 
+				if (medidas.equalsIgnoreCase("centimetros")) {
+					System.out.println("Introduzca la distancia en pulgadas");
+					distancia = sc.nextInt();
+					System.out.println("Son " + centimetros(distancia) + " centimetros");
+
+				} else if (medidas.equalsIgnoreCase("pulgadas")) {
+
+					System.out.println("Introduzca la distancia en centímetros");
+					distancia = sc.nextInt();
+					System.out.println("Son " + pulgadas(distancia) + " pulgadas");
+				}
+				break;
+
+			case "Salir":
+
+				System.out.println("Cerrando aplicación");
+				fin = true;
+				break;
 			default:
+				System.out.println("Operación desconocida");
 				break;
 			}
 
 		} while (!fin);
 
+		System.out.println("Gracias por usar esta aplicación");
 		sc.close();
 	}
 
