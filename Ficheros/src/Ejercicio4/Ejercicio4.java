c0e86583f8757659d135a2714cafa059b052af17
DrunkPsyduck <40211718+DrunkPsyduck@users.noreply.github.com>
2019-04-08 20:28:31 +0200

    s
    451345

--- Ficheros/src/Ejercicio4/Ejercicio4.java
+++ Ficheros/src/Ejercicio4/Ejercicio4.java
@@ -1,4 +1,26 @@
 package Ejercicio4;
-
+import java.io.*;
 public class Ejercicio4 {
+
+    static String ruta = "../Ficheros/datos.txt";
+
+    public static void leerArchivo() throws IOException{
+        File archivo = new File(ruta);
+
+        BufferedReader br = new BufferedReader(new FileReader(archivo));
+
+        String texto = br.readLine();
+        System.out.println(texto);
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            leerArchivo();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+
+        }
+    }
+
 }
